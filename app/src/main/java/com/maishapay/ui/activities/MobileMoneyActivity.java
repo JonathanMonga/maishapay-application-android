cab125a6795ffad1fd9a5e84e5b9a8b620eee90b
JonathanMonga <jmonga98@gmail.com>
2018-11-13 15:18:17 +0100

    commit

--- app/src/main/java/com/maishapay/ui/activities/MobileMoneyActivity.java
+++ app/src/main/java/com/maishapay/ui/activities/MobileMoneyActivity.java
@@ -19,6 +19,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.afollestad.materialdialogs.MaterialDialog;
 import com.maishapay.R;
@@ -44,6 +45,8 @@ public class MobileMoneyActivity extends AppCompatActivity {
     private static final String AFRICELL_OPERATOR = "africell";
     private static final String VODACOM_OPERATOR = "VODACOM CD";
     private static final String ORANGE_OPERATOR = "Orange";
+    private static final String TIGO_OPERATOR = "Tigo";
+    private static final String EXPRESSO_OPERATOR = "Expresso";
 
     private String country;
 
@@ -63,6 +66,23 @@ public class MobileMoneyActivity extends AppCompatActivity {
     ImageView money3;
     @BindView(R.id.money4)
     ImageView money4;
+    @BindView(R.id.nom1)
+    TextView nom1;
+    @BindView(R.id.nom2)
+    TextView nom2;
+    @BindView(R.id.nom3)
+    TextView nom3;
+    @BindView(R.id.nom4)
+    TextView nom4;
+    @BindView(R.id.description1)
+    TextView description1;
+    @BindView(R.id.description2)
+    TextView description2;
+    @BindView(R.id.description3)
+    TextView description3;
+    @BindView(R.id.description4)
+    TextView description4;
+
     @BindView(R.id.toolbar_actionbar)
     Toolbar toolbar;
 
@@ -87,35 +107,99 @@ public class MobileMoneyActivity extends AppCompatActivity {
 
         switch (codePhone) {
             case "+211": {
+                money1.setImageResource(R.drawable.tigo);
+                nom1.setText("");
+                description1.setText("");
 
+                money2.setImageResource(R.drawable.expresso);
+                nom2.setText("");
+                description2.setText("");
+
+                money4.setImageResource(R.drawable.money4);
+                nom4.setText("");
+                description4.setText("");
+
+                cardMoneyId3.setVisibility(View.GONE);
                 break;
             }
 
             case "+250": {
+                money1.setImageResource(R.drawable.tigo);
+                nom1.setText("");
+                description1.setText("");
 
+                money2.setImageResource(R.drawable.expresso);
+                nom2.setText("");
+                description2.setText("");
+
+                cardMoneyId3.setVisibility(View.GONE);
+                cardMoneyId4.setVisibility(View.GONE);
                 break;
             }
 
             case "+228": {
+                money1.setImageResource(R.drawable.tigo);
+                nom1.setText("");
+                description1.setText("");
 
+                money2.setImageResource(R.drawable.expresso);
+                nom2.setText("");
+                description2.setText("");
+
+                cardMoneyId3.setVisibility(View.GONE);
+                cardMoneyId4.setVisibility(View.GONE);
                 break;
             }
 
             case "+225": {
+                money1.setImageResource(R.drawable.money1);
+                nom1.setText("");
+                description1.setText("");
 
+                money2.setImageResource(R.drawable.money2);
+                nom2.setText("");
+                description2.setText("");
+
+                money3.setImageResource(R.drawable.money3);
+                nom3.setText("");
+                description3.setText("");
+
+                money4.setImageResource(R.drawable.money4);
+                nom4.setText("");
+                description4.setText("");
                 break;
             }
 
             case "+254": {
+                money1.setImageResource(R.drawable.tigo);
+                nom1.setText("");
+                description1.setText("");
 
+                money2.setImageResource(R.drawable.expresso);
+                nom2.setText("");
+                description2.setText("");
+
+                cardMoneyId3.setVisibility(View.GONE);
+                cardMoneyId4.setVisibility(View.GONE);
                 break;
             }
 
             default: {
                 money1.setImageResource(R.drawable.money1);
+                nom1.setText("M-pesa");
+                description1.setText("Service mobile money pour Vodacom.");
+
                 money2.setImageResource(R.drawable.money2);
+                nom2.setText("Africell money");
+                description2.setText("Service mobile money pour Africell.");
+
                 money3.setImageResource(R.drawable.money3);
+                nom3.setText("Airtel money");
+                description3.setText("Service mobile money pour Airtel.");
+
                 money4.setImageResource(R.drawable.money4);
+                nom4.setText("Orange money");
+                description4.setText("Service mobile money pour Orange.");
             }
         }
     }
@@ -136,7 +220,26 @@ public class MobileMoneyActivity extends AppCompatActivity {
 
         switch (codePhone) {
             case "+211": {
-
+                if (Constants.getOperatorNamePhone().equals(TIGO_OPERATOR)) {
+                    Intent callIntent = new Intent(Intent.ACTION_CALL, ussdToCallableUri("*1222#"));
+                    if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                        // TODO: Consider calling
+                        //    ActivityCompat#requestPermissions
+                        // here to request the missing permissions, and then overriding
+                        //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                        //                                          int[] grantResults)
+                        // to handle the case where the user grants the permission. See the documentation
+                        // for ActivityCompat#requestPermissions for more details.
+                        return;
+                    }
+                    startActivity(callIntent);
+                } else
+                    Snacky.builder()
+                            .setView(findViewById(R.id.root))
+                            .setText("Vous utilisez un autre operateur.")
+                            .setDuration(Snacky.LENGTH_LONG)
+                            .warning()
+                            .show();
                 break;
             }
 
@@ -191,7 +294,26 @@ public class MobileMoneyActivity extends AppCompatActivity {
 
         switch (codePhone) {
             case "+211": {
-
+                if (Constants.getOperatorNamePhone().equals(EXPRESSO_OPERATOR)) {
+                    Intent callIntent = new Intent(Intent.ACTION_CALL, ussdToCallableUri("*1222#"));
+                    if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                        // TODO: Consider calling
+                        //    ActivityCompat#requestPermissions
+                        // here to request the missing permissions, and then overriding
+                        //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                        //                                          int[] grantResults)
+                        // to handle the case where the user grants the permission. See the documentation
+                        // for ActivityCompat#requestPermissions for more details.
+                        return;
+                    }
+                    startActivity(callIntent);
+                } else
+                    Snacky.builder()
+                            .setView(findViewById(R.id.root))
+                            .setText("Vous utilisez un autre operateur.")
+                            .setDuration(Snacky.LENGTH_LONG)
+                            .warning()
+                            .show();
                 break;
             }
 
